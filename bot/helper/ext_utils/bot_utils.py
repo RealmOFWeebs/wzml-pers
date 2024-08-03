600df91e35cb2faf8c12cec2a0b4c5fb84bbe91c
RealmOFWeebs <siddharth.075@proton.me>
2024-08-03 09:04:55 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -156,8 +156,8 @@ def handleIndex(index, dic):
 def get_progress_bar_string(pct):
     pct = float(str(pct).strip('%'))
     p = min(max(pct, 0), 100)
-    cFull = int(p // 8)
-    cPart = int(p % 8 - 1)
+    cFull = int(p // 10)
+    cPart = int(p % 10 - 1)
     p_str = '⬢' * cFull
     if cPart >= 0:
         p_str += ['⬡', '⬡', '⬡', '⬡', '⬡', '⬡', '⬢'][cPart]
