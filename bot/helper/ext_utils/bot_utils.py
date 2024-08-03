73ded0e5ca996efe3086f4ae94c54fdb31e9551f
RealmOFWeebs <siddharth.075@proton.me>
2024-08-03 10:06:34 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -161,7 +161,7 @@ def get_progress_bar_string(pct):
     p_str = '⬢' * cFull
     if cPart >= 0:
         p_str += ['⬡', '⬡', '⬡', '⬡', '⬡', '⬡', '⬢'][cPart]
-    p_str += '⬡' * (11 - cFull)
+    p_str += '⬡' * (12 - cFull)
     return f"[{p_str}]"
 
 
